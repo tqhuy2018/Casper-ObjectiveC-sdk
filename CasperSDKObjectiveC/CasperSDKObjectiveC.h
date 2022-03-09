b019fdcb8bf8e1be0a1aa5140e55737b08b31720
hienbui9999 <94465107+hienbui9999@users.noreply.github.com>
2022-03-09 12:39:52 +0700

    Initialize the project
    Initialize the project

---
+++ CasperSDKObjectiveC/CasperSDKObjectiveC.h
@@ -0,0 +1,18 @@
+//
+//  CasperSDKObjectiveC.h
+//  CasperSDKObjectiveC
+//
+//  Created by Hien on 09/03/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CasperSDKObjectiveC.
+FOUNDATION_EXPORT double CasperSDKObjectiveCVersionNumber;
+
+//! Project version string for CasperSDKObjectiveC.
+FOUNDATION_EXPORT const unsigned char CasperSDKObjectiveCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CasperSDKObjectiveC/PublicHeader.h>
+
+
