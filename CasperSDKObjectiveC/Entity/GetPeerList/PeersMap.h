cd84615f619449ffe8acf06df294f39e0c564f8a
hienbui9999 <buiduyhien@gmail.com>
2022-03-12 20:51:34 +0700

    Adjust some code

---
+++ CasperSDKObjectiveC/Entity/GetPeerList/PeersMap.h
@@ -0,0 +1,8 @@
+#ifndef PeersMap_h
+#define PeersMap_h
+#import "PeerEntry.h"
+@interface PeersMap
+@property NSMutableArray * listPeerEntry
+@end
+
+#endif 
---
+++ CasperSDKObjectiveC/Entity/GetStatus/GetStatusResult.h
@@ -0,0 +1,11 @@
+#ifndef GetStatusResult_h
+#define GetStatusResult_h
+
+@interface GetStatusResult : NSObject
+@property NSString * api_version;
+@property NSString * chainspec_name;
+@property NSString * starting_state_root_hash;
+
+@end
+
+#endif
