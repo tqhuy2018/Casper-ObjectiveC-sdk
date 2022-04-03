5e7b8e4a64a0edb9d3811688d3e0b3a9d7f9f35d
hienbui9999 <buiduyhien@gmail.com>
2022-04-03 08:29:53 +0700

    Add comments

--- CasperSDKObjectiveC/Entity/GetDictionaryItem/GetDictionaryItemParams.m
+++ CasperSDKObjectiveC/Entity/GetDictionaryItem/GetDictionaryItemParams.m
@@ -7,7 +7,14 @@
 #import "DictionaryIdentifier_ContractNamedKey.h"
 #import "DictionaryIdentifier_URef.h"
 #import "DictionaryIdentifier_Dictionary.h"
+/**Class built for storing GetBalanceParams information, used for sending state_get_dictionary_item RPC method
+ */
 @implementation GetDictionaryItemParams
+/**This function generate the json data used for sending POST method for state_get_dictionary_item RPC call.
+Based on the GetDictionaryItemParams data, the JSON string is built somehow like this:
+ 
+ {"method" : "state_get_dictionary_item","id" : 1,"params" :{"state_root_hash" : "146b860f82359ced6e801cbad31015b5a9f9eb147ab2a449fd5cdb950e961ca8","dictionary_identifier":{"AccountNamedKey":{"dictionary_name":"dict_name","key":"account-hash-ad7e091267d82c3b9ed1987cb780a005a550e6b3d1ca333b743e2dba70680877","dictionary_item_key":"abc_name"}}},"jsonrpc" : "2.0"}
+ */
 -(NSString *) toJsonString {
     NSString * ret = @"";
     //AccountNamedKey
--- CasperSDKObjectiveC/Entity/GetDictionaryItem/GetDictionaryItemResult.m
+++ CasperSDKObjectiveC/Entity/GetDictionaryItem/GetDictionaryItemResult.m
@@ -2,7 +2,11 @@
 #import "GetDictionaryItemResult.h"
 #import "ConstValues.h"
 #import "HttpHandler.h"
+/**Class built for storing GetDictionaryItemResult information, taken from state_get_dictionary_item RPC method
+ */
 @implementation GetDictionaryItemResult
+/**This function parse the Dictionary object (as part of the JSON object taken from server RPC method call) to GetDictionaryItemResult object
+ */
 +(GetDictionaryItemResult*) fromJsonDictToGetItemResult:(NSDictionary*) fromDict {
     GetDictionaryItemResult * ret = [[GetDictionaryItemResult alloc] init];
     ret.api_version = (NSString* ) fromDict[@"api_version"];
@@ -11,6 +15,13 @@
     ret.stored_value = [StoredValue fromJsonDictToStoredValue:(NSDictionary*) fromDict[@"stored_value"]];
     return ret;
 }
+/**This function initiate the process of sending POST request with given parameter in JSON string format
+The input jsonString is used to send to server as parameter of the POST request to get the result back
+The input jsonString is somehow like this:
+ 
+ {"method" : "state_get_dictionary_item","id" : 1,"params" :{"state_root_hash" : "146b860f82359ced6e801cbad31015b5a9f9eb147ab2a449fd5cdb950e961ca8","dictionary_identifier":{"AccountNamedKey":{"dictionary_name":"dict_name","key":"account-hash-ad7e091267d82c3b9ed1987cb780a005a550e6b3d1ca333b743e2dba70680877","dictionary_item_key":"abc_name"}}},"jsonrpc" : "2.0"}
+ 
+ */
 +(void) getDictionaryItemWithParams:(NSString*) jsonString {
     [HttpHandler handleRequestWithParam:jsonString andRPCMethod:CASPER_RPC_METHOD_STATE_GET_DICTIONARY_ITEM];
 }
