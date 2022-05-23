7864a1b631aef180bffbb4d6556ee5c04bebfb4d
hienbui9999 <94465107+hienbui9999@users.noreply.github.com>
2022-05-23 16:01:23 +0700

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.1
+
+import Foundation
+import PackageDescription
+let package = Package(
+    name: "CasperSDKObjectiveC",
+    products: [
+        .library(name: "CasperSDKObjectiveC", targets: ["CasperSDKObjectiveC"])
+    ],
+    targets: [
+        .target(
+            name: "CasperSDKObjectiveC",
+            path: "CasperSDKObjectiveC",
+            exclude: [
+                "../CasperSDKObjectiveCTests/GetStateRootHashTest.m",
+                "../CasperSDKObjectiveCTests/GetPeerResultTest.m",
+                "../CasperSDKObjectiveCTests/GetStatusTest.m",
+                "../CasperSDKObjectiveCTests/GetBlockTransfersResultTest.m",
+                "../CasperSDKObjectiveCTests/GetBlockTest.m"
+            ],
+            cSettings: [
+                .headerSearchPath("UI"),
+            ]
+        )
+    ]
+)
