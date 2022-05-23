482b82f3bd52c408e2a93be07682cd0777d8e94a
hienbui9999 <94465107+hienbui9999@users.noreply.github.com>
2022-05-23 16:49:36 +0700

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CasperSDKObjectiveC",
+    platforms: [
+        .iOS(.v13), .tvOS(.v14), .watchOS(.v7), .macOS(.v11)
+        ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "CasperSDKObjectiveC",
+            targets: ["CasperSDKObjectiveC"]),
+    ],
+    dependencies: [
+        .package(name: "Blake2", url: "https://github.com/tesseract-one/Blake2.swift.git", from: "0.1.0"),
+        .package(name: "SwiftECC", url: "https://github.com/leif-ibsen/SwiftECC", from: "3.3.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "CasperSDKObjectiveC",
+            dependencies: ["Blake2", "SwiftECC","secp256k1"],
+           
+                ),
+        .testTarget(
+            name: "CasperSDKObjectiveCTests",
+            dependencies: ["CasperSDKObjectiveC", "Blake2", "SwiftECC","secp256k1"]),
+    ]
+)
