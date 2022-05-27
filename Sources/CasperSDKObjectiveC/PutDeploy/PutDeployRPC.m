f79ed2cd6bf178f14ad7d484719ec16236611fc1
hienbui9999 <buiduyhien@gmail.com>
2022-05-27 16:12:13 +0700

    a

--- Sources/CasperSDKObjectiveC/PutDeploy/PutDeployRPC.m
+++ Sources/CasperSDKObjectiveC/PutDeploy/PutDeployRPC.m
@@ -32,20 +32,26 @@
     NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:config delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
     NSURLSessionDataTask * task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-        NSDictionary *forJSONObject = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
-        CasperErrorMessage * cem = [[CasperErrorMessage alloc] init];
-        [cem fromJsonToErrorObject:forJSONObject];
-        if(cem.message == CASPER_ERROR_MESSAGE_NONE) {
-            PutDeployResult * ret = [[PutDeployResult alloc] init];
-            ret = [PutDeployResult fromJsonObjectToPutDeployResult:(NSDictionary*) forJSONObject[@"result"]];
-            NSLog(@"Put deploy success with deploy hash:%@",ret.deployHash);
-            PutDeployUtils.putDeployCounter = 0;
-            PutDeployUtils.isPutDeploySuccess = true;
-        } else {
-            NSLog(@"Error put deploy with error message:%@ and error code:%@",cem.message,cem.code);
-            if([cem.message isEqualToString: @"invalid deploy: the approval at index 0 is invalid: asymmetric key error: failed to verify secp256k1 signature: signature error"]) {
-                PutDeployUtils.isPutDeploySuccess = false;
+        NSHTTPURLResponse * httpResponse = (NSHTTPURLResponse *)response;
+        if(httpResponse.statusCode == 200)
+        {
+            NSDictionary *forJSONObject = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
+            CasperErrorMessage * cem = [[CasperErrorMessage alloc] init];
+            [cem fromJsonToErrorObject:forJSONObject];
+            if(cem.message == CASPER_ERROR_MESSAGE_NONE) {
+                PutDeployResult * ret = [[PutDeployResult alloc] init];
+                ret = [PutDeployResult fromJsonObjectToPutDeployResult:(NSDictionary*) forJSONObject[@"result"]];
+                NSLog(@"Put deploy success with deploy hash:%@",ret.deployHash);
+                PutDeployUtils.putDeployCounter = 0;
+                PutDeployUtils.isPutDeploySuccess = true;
+            } else {
+                NSLog(@"Error put deploy with error message:%@ and error code:%@",cem.message,cem.code);
+                if([cem.message isEqualToString: @"invalid deploy: the approval at index 0 is invalid: asymmetric key error: failed to verify secp256k1 signature: signature error"]) {
+                    PutDeployUtils.isPutDeploySuccess = false;
+                }
             }
+        } else {
+            
         }
     }];
     if(PutDeployUtils.isPutDeploySuccess == false) {
@@ -75,25 +81,33 @@
     NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
     NSURLSession * session = [NSURLSession sessionWithConfiguration:config delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
     NSURLSessionDataTask * task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-        NSDictionary *forJSONObject = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
-        NSLog(@"After putting,  value back is:%@",forJSONObject);
-        CasperErrorMessage * cem = [[CasperErrorMessage alloc] init];
-        [cem fromJsonToErrorObject:forJSONObject];
-        if(cem.message == CASPER_ERROR_MESSAGE_NONE) {
-            PutDeployResult * ret = [[PutDeployResult alloc] init];
-            ret = [PutDeployResult fromJsonObjectToPutDeployResult:(NSDictionary*) forJSONObject[@"result"]];
-            NSLog(@"Put deploy success with deploy hash:%@",ret.deployHash);
-            PutDeployUtils.putDeployCounter = 0;
-            PutDeployUtils.isPutDeploySuccess = true;
-            self.rpcCallGotResult[callID] = RPC_VALID_RESULT;
-            self.valueDict[callID] = ret;
-        } else {
-            NSLog(@"Error put deploy with error message:%@ and error code:%@",cem.message,cem.code);
-            if([cem.message isEqualToString: @"invalid deploy: the approval at index 0 is invalid: asymmetric key error: failed to verify secp256k1 signature: signature error"]) {
-                PutDeployUtils.isPutDeploySuccess = false;
+        NSHTTPURLResponse * httpResponse = (NSHTTPURLResponse *)response;
+        if(httpResponse.statusCode == 200)
+        {
+            NSDictionary *forJSONObject = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
+            NSLog(@"After putting,  value back is:%@",forJSONObject);
+            CasperErrorMessage * cem = [[CasperErrorMessage alloc] init];
+            [cem fromJsonToErrorObject:forJSONObject];
+            if(cem.message == CASPER_ERROR_MESSAGE_NONE) {
+                PutDeployResult * ret = [[PutDeployResult alloc] init];
+                ret = [PutDeployResult fromJsonObjectToPutDeployResult:(NSDictionary*) forJSONObject[@"result"]];
+                NSLog(@"Put deploy success with deploy hash:%@",ret.deployHash);
+                PutDeployUtils.putDeployCounter = 0;
+                PutDeployUtils.isPutDeploySuccess = true;
+                self.rpcCallGotResult[callID] = RPC_VALID_RESULT;
+                self.valueDict[callID] = ret;
             } else {
-                self.rpcCallGotResult[callID] = RPC_VALUE_ERROR_OBJECT;
+                NSLog(@"Error put deploy with error message:%@ and error code:%@",cem.message,cem.code);
+                if([cem.message isEqualToString: @"invalid deploy: the approval at index 0 is invalid: asymmetric key error: failed to verify secp256k1 signature: signature error"]) {
+                    PutDeployUtils.isPutDeploySuccess = false;
+                } else {
+                    self.rpcCallGotResult[callID] = RPC_VALUE_ERROR_OBJECT;
+                    NSLog(@"Error put wrong value");
+                }
             }
+        } else {
+            self.rpcCallGotResult[callID] = RPC_VALUE_ERROR_NETWORK;
+            NSLog(@"Error because of network");
         }
     }];
     if(PutDeployUtils.isPutDeploySuccess == false) {
