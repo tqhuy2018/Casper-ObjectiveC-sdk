43bb71ebd3786942f47fb00b88c1e79dd5bc7917
hienbui9999 <buiduyhien@gmail.com>
2022-06-03 07:46:46 +0700

    v1.0.0

--- Sources/CasperSDKObjectiveC/GetAuctionInfo/GetAuctionInfoResult.m
+++ Sources/CasperSDKObjectiveC/GetAuctionInfo/GetAuctionInfoResult.m
@@ -16,7 +16,6 @@
 /**This function initiate the process of sending POST request with given parameter in JSON string format
 The input jsonString is used to send to server as parameter of the POST request to get the result back
 The input jsonString is somehow like this:
- 
  {"method" : "state_get_auction_info","id" : 1,"params" : {"block_identifier" : {"Hash" :"d16cb633eea197fec519aee2cfe050fe9a3b7e390642ccae8366455cc91c822e"}},"jsonrpc" : "2.0"}
  */
 +(void) getAuctionWithParams:(NSString*) jsonString {
--- Sources/CasperSDKObjectiveC/GetBlock/GetBlockResult.m
+++ Sources/CasperSDKObjectiveC/GetBlock/GetBlockResult.m
@@ -17,9 +17,7 @@
 /**This function initiate the process of sending POST request with given parameter in JSON string format
 The input jsonString is used to send to server as parameter of the POST request to get the result back
 The input jsonString is somehow like this:
- 
  {"method" : "chain_get_block","id" : 1,"params" : {"block_identifier" : {"Hash" :"d16cb633eea197fec519aee2cfe050fe9a3b7e390642ccae8366455cc91c822e"}},"jsonrpc" : "2.0"}
- 
  */
 +(void) getBlockWithParams:(NSString*) jsonString {
     [HttpHandler handleRequestWithParam:jsonString andRPCMethod:CASPER_RPC_METHOD_CHAIN_GET_BLOCK];
